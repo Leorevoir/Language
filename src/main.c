d6c8ada6ecd3ebefc67f9ee8ddf809ec44839268
Leorevoir <leo.quinzler@epitech.eu>
2025-08-14 13:24:09 +0200

    add: cplus base repo

--- src/main.c
+++ src/main.c
@@ -1,13 +1,26 @@
-#include <rune/compiler/ast.h>
-#include <rune/memory.h>
+#include "cplus/array.h"
+#include "std/memory/garbage_collector.h"
+#include <stdio.h>
 
-int main(void)
+typedef struct {
+    float x;
+    float y;
+} vec2f;
+
+static void print_vec2f(const vec2f *v)
 {
-    auto_clean AST *ast = new (ASTClass, "examples/example.rune");
+    printf("vec2f(%f, %f)\n", v->x, v->y);
+}
 
-    ast->collect_tokens(ast);
-    ast->build(ast);
-    ast->show(ast);
+int main(const int argc, const char **argv)
+{
+    Array *array = (Array *) new (ArrayClass, sizeof(any), 10, NULL);
+    const vec2f v1 = {0.1f, 0.2f};
 
-    return 0;
+    array->append(array, &v1);
+    array->append(array, &(vec2f) {0.3f, 0.4f});
+    array->append(array, &(vec2f) {0.5f, 0.6f});
+
+    array_foreach(array, vec2f, v, { print_vec2f(v); });
+    delete (array);
 }
